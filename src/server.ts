b0cd5a80d359cff49a4ec6b9681fd268494402d3
Jose Hernandez <h2jose@gmail.com>
2020-10-19 11:43:10 -0400

    init querys, resolvers y schema

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,41 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { IResolvers, makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
 
 const app = express();
 
 app.use('*', cors());
 
 app.use(compression());
+
+const typeDefs = `
+	type Query {
+		hola: String!
+		holaConNombre(nombre: String!): String!
+		holaAlApiGraphQL: String!
+	}
+`
+const resolvers: IResolvers = {
+	Query : {
+		hola(): string{
+			return 'Hola Mundo';
+		},
+		holaConNombre(__: void, { nombre }): string {
+			return `Hola mundo ${nombre}`;
+		},
+		holaAlApiGraphQL(): string {
+			return "Hola Mundo desde Api GraphQL test";
+		}
+	}
+}
+
+const schema: GraphQLSchema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+});
+
 
 app.use('/', (req,res) => {
 	res.send('Welcome to Express Response end point...');
